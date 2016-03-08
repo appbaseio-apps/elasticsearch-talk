5629a603556ca2d0f0f35244e825bcf07c120a12
Farhan Chauhan <farhan687@gmail.com>
2016-03-08 18:12:10 +0530

    Changed for analyzed

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
-	"app_name": "App3",
-	"username": "CnqEgei0f",
-	"password": "a2176969-de4c-4ed0-bbbe-67e152de04f7"
+	"username": "xq6jAecwD",
+	"app_name": "demo-2",
+	"password": "760ebc72-2354-4e70-ab94-5d6e6b3d9605"
 }
